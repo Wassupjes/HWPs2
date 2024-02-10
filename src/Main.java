036e995c6179e3ccdade5578a24030f9ea0ad0ec
Alexey <wassupjes@mail.ru>
2024-02-10 16:17:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 100;
+        int refill = 1200;
+
+        int bonus = 0;
+        if (refill > 1000) {
+            bonus = refill / 100;
+        }
+
+        int finalBalance = initialBalance + refill + bonus;
+        System.out.println("Бонус: " + bonus);
+        System.out.println("Итоговый баланс: " + finalBalance);
+        }
+    }
